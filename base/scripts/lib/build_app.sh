89523dfd8fc8c823bdb9b0d20dca99e5fd7a9020
Mitja Jež <mitja.jez@gmail.com>
2016-06-30 12:06:08 +0200

    Update build_app.sh
    --production flag just for last npm install (inspired by [meteor-buildpack-horse] (https://github.com/AdmitHub/meteor-buildpack-horse/commit/1c1de57e9e9cf3861b9a5f3ff101e2bbc3b7d645) ).

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -6,12 +6,12 @@ BUNDLE_DIR=/tmp/bundle-dir
 # sometimes, directly copied folder cause some wierd issues
 # this fixes that
 cp -R /app $COPIED_APP_PATH
-cd $COPIED_APP_PATH && npm install --production
+cd $COPIED_APP_PATH && npm install
 
 meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
 
 cd $BUNDLE_DIR/bundle/programs/server/
-npm i
+npm install --production
 
 mv $BUNDLE_DIR/bundle /built_app
 
